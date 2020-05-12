35ab8659e03a8c22583a4bd26c4a12d7dbb25f3a
elenisproject <elenis.karanikola@gmail.com>
2020-05-12 22:15:06 +0300

    create_table

--- article_table.sql
+++ article_table.sql
@@ -1,8 +1,8 @@
 USE NewsCrawler;
 CREATE TABLE articles(
-	subtopic VARCHAR(50),
+    subtopic VARCHAR(50),
     website CHAR(20),
-	title TEXT,
+    title TEXT,
     article_date VARCHAR(50),
     author VARCHAR(50),
     article_body LONGTEXT,
