21fc52acb4a79d97c33a297a6540e3396a022f51
elenisproject <elenis.karanikola@gmail.com>
2020-11-19 17:55:36 +0200

    API_similar

--- Database_Configuration/similar_articles.sql
+++ Database_Configuration/similar_articles.sql
@@ -1,7 +1,11 @@
 USE NewsCrawler;
 create table similar_articles(
    id INT NOT NULL,
-   similar_articles VARCHAR(100) NOT NULL,
+   first_article INT NOT NULL,
+   second_article INT NOT NULL,
+   third_article INT NOT NULL,
+   fourth_article INT NOT NULL,
+   fifth_article INT NOT NULL,
    PRIMARY KEY ( id ),
    FOREIGN KEY (id) REFERENCES articles(id)
 	ON DELETE CASCADE
